21f2275ce1af7336db458218077bfe957277c5b6
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-08 17:48:43 -0700

    Methods,java

--- src/extra/Methods.java
+++ src/extra/Methods.java
@@ -1,5 +1,89 @@
-package extra;
 
-public @interface Methods {
+/*
+ * 1. Call this method so that it prints “I like apples”. 
+
+
+void food(String fruit) {Hell
+
+       System.out.println("I like " + fruit); 
+       
+}
+
+
+Something.food("apples");
+
+
+2. Call this method to that it prints “Nerds rule”.
+
+
+void truth(int x) {
+
+       if(x > 5) 
+
+              System.out.println("Geeks rule");
+
+       else
+
+              System.out.println("Nerds rule");
 
 }
+
+school.truth(4);
+
+
+3. Use this method to draw a square. Don’t change the inside of the method.
+
+
+void drawLine() {
+
+       Tortoise.move(100);
+
+       Tortoise.turn(90); 
+}
+
+for(int i=0;i<4;i++){
+Tortoise.drawLine();
+}
+
+
+4. Fix any problems with the following constructors.
+
+
+class Hell {
+
+      void temperature(int hotness) {
+
+
+
+      }
+
+}
+
+public class Bacon {
+
+      String style = "streaky";
+
+      Bacon (String style) {
+            style = style;
+      }
+
+}
+
+public class Unicorn {
+
+
+      private Unicorn() {
+            this.horns = 1;
+      }
+
+}
+
+class Narwhal {
+
+      public Narwhale(){}
+
+}
+
+ * 
+ */
+
